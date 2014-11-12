54a0450b48df92ded0b1e07a1dfce58eb9699e69
lbowles <lbowles@LT690069.SITES.ENTPLC.NET>
2014-11-12 15:44:06 +0000

    calabash run update

--- android-ci/calabash/run.bat
+++ android-ci/calabash/run.bat
@@ -3,6 +3,7 @@ TITLE Calabash AppCreator console
 ECHO OFF 
 ECHO Calabash test for AppCreator Store.
 ::
+C:\Program Files\Jenkins\jobs\android-ci\workspace\android-ci\calabash
 calabash-android run ..\bin\MainActivity-debug.apk  --format html --out reports.html TEST_SERVER_PORT=38002 -v
 ::
 ECHO All done.

492017ff7cc8857b6c28b4f394a122dcaa19f8c5
lbowles <lbowles@LT690069.SITES.ENTPLC.NET>
2014-11-13 08:32:58 +0000

    calabash run path fix

--- android-ci/calabash/run.bat
+++ android-ci/calabash/run.bat
@@ -3,8 +3,8 @@ TITLE Calabash AppCreator console
 ECHO OFF 
 ECHO Calabash test for AppCreator Store.
 ::
-cd C:\Program Files\Jenkins\jobs\android-ci\workspace\android-ci\calabash
-calabash-android resign C:/Program Files/Jenkins/jobs/android-ci/workspace/android-ci/bin/MainActivity-debug.apk
+cd "C:\Program Files\Jenkins\jobs\android-ci\workspace\android-ci\calabash"
+calabash-android resign ..\bin\MainActivity-debug.apk"
 calabash-android run ..\bin\MainActivity-debug.apk  --format html --out reports.html TEST_SERVER_PORT=38002 -v
 ::
 ECHO All done.
